6a69c1c0b0a0d82ab35ace6f166403cad1a241b9
Yana Ivanova <y.m.ivanova23@gmail.com>
2016-02-25 00:38:08 +0200

    Sorting added

--- js/Shop.js
+++ js/Shop.js
@@ -2,20 +2,20 @@
  * Created by Yana on 2/24/2016.
  */
 
-var Shop = (function (){
-    function Shop () {
+var Shop = (function () {
+    function Shop() {
         var _cash = 1000;
         var products = [];
 
-        this.getCash = function() {
+        this.getCash = function () {
             return _cash;
         }
 
-        this.setCash = function(cash) {
+        this.setCash = function (cash) {
             _cash += cash;
         }
 
-        this.getProducts = function (){
+        this.getProducts = function () {
             return products;
         }
 
@@ -29,22 +29,56 @@ var Shop = (function (){
         var index = this.getProducts().indexOf(item);
         var itemToRemove = this.getProducts()[index];
         itemToRemove.setQuantity(-quantity);
-        this.setCash((itemToRemove.getPrice())*quantity);
+        this.setCash((itemToRemove.getPrice()) * quantity);
     }
 
     Shop.prototype.receiveProducts = function (provider, item, quantity) {
         provider.deliverItem(item, quantity);
     }
 
-    //Shop.prototype.sortProductsByName = function () {
-    //    for ()
-    //}
-    //
-    //objs.sort(compare);
-    //}
-    Shop.prototype.reviewItems = function (type) {
-        if (type == 'alphabetic') {
+    Shop.prototype.sortProductsByPrice = function () {
+        this.getProducts().sort(function (a, b) {
+            return parseFloat(a.getPrice()) - parseFloat(b.getPrice());
+        });
+    }
 
+    Shop.prototype.sortProductsByName = function (a, b) {
+        if (a.getName() < b.getName())
+            return -1;
+        else if (a.getName() > b.getName())
+            return 1;
+        else
+            return 0;
+    }
+
+    Shop.prototype.sortProductsByType = function (a, b) {
+        if (a.getType() < b.getType())
+            return -1;
+        else if (a.getType() > b.getType())
+            return 1;
+        else
+            return 0;
+    }
+
+
+    Shop.prototype.reviewItems = function (type) {
+        if (type == 'byName') {
+            this.getProducts().sort(this.sortProductsByName);
+            for (var i = 0; i < this.getProducts().length; i++) {
+                console.log('Name : ' + this.getProducts()[i].getName());
+            }
+        }
+        if (type == 'byPrice') {
+            this.sortProductsByPrice();
+            for (var i = 0; i < this.getProducts().length; i++) {
+                console.log(this.getProducts()[i].getName() + ' price - ' + this.getProducts()[i].getPrice());
+            }
+        }
+        if (type == 'byType') {
+            this.getProducts().sort(this.sortProductsByType);
+            for (var i = 0; i < this.getProducts().length; i++) {
+                console.log(this.getProducts()[i].getName() + ' type - ' + this.getProducts()[i].getType());
+            }
         }
     }
 
